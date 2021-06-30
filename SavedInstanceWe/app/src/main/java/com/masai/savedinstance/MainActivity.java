12c7341f242e4d91d24479ba51f7c850379246df
leena-sunkara <86671330+leena-sunkara@users.noreply.github.com>
2021-06-30 17:46:37 +0530

    version 1.0

---
+++ SavedInstanceWe/app/src/main/java/com/masai/savedinstance/MainActivity.java
@@ -0,0 +1,34 @@
+package com.masai.savedinstance;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTvSaveInstance;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mTvSaveInstance = findViewById(R.id.tvSaveInstance);
+        mTvSaveInstance.setText("Welcome to Masai School");
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("key", "Screen Rotated");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        String data = savedInstanceState.getString("key");
+        mTvSaveInstance.setText(data);
+    }
+}
